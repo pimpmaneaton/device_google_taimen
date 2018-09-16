729ad2e0b3881bb927ef07e1a9a85a9d02528c03
pimpmaneaton <shaunsibu@gmail.com>
2018-09-15 19:44:51 -0600

    Change to BLiss

--- bliss_taimen.mk
+++ bliss_taimen.mk
@@ -13,7 +13,7 @@
 # limitations under the License.
 
 # Include DU common configuration
-include vendor/du/config/common_full_phone.mk
+include vendor/bliss/config/common_full_phone.mk
 
 # Inherit from the common Open Source product configuration
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
@@ -24,7 +24,7 @@ $(call inherit-product, device/google/taimen/aosp_taimen.mk)
 
 
 # Override AOSP build properties
-PRODUCT_NAME := du_taimen
+PRODUCT_NAME := bliss_taimen
 PRODUCT_DEVICE := taimen
 PRODUCT_BRAND := Google
 PRODUCT_MODEL := Pixel 2 XL
