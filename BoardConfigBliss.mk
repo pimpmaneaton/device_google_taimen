064b9b2f0fcd499616e63463499294838640b704
Ethan Chen <intervigil@gmail.com>
2018-09-16 14:06:26 -0600

    taimen: Compile GPS HAL from hardware/qcom/gps

---
+++ BoardConfigBliss.mk
@@ -0,0 +1,8 @@
+
+# Custom board config for taimen
+# Kernel defconfig
+TARGET_KERNEL_CONFIG := custom_defconfig
+
+# GPS
+BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := default
+BOARD_VENDOR_QCOM_LOC_PDK_FEATURE_SET := true
