ee2b509b992584e99a558b476bf4571a16ec8cf8
pimpmaneaton <shaunsibu@gmail.com>
2019-02-03 09:39:11 -0700

    Add system props for IMS features
    * This fixes IMS for various carriers by allowing /system to access
      these prop calls.
    
    Credit to Toucan-Sam for his work in VoEnabler Magisk Module

--- device-bliss.mk
+++ device-bliss.mk
@@ -23,6 +23,17 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     net.tethering.noprovisioning=true
 
+# system props for ims features
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.dbg.ims_volte_enable=1 \
+    persist.dbg.volte_avail_ovr=1 \
+    persist.dbg.vt_avail_ovr=1 \
+    persist.dbg.wfc_avail_ovr=1 \
+    persist.radio.rat_on=combine \
+    persist.radio.data_ltd_sys_ind=1 \
+    persist.radio.data_con_rprt=1 \
+    persist.radio.calls.on.ims=1
+
 # UI
 PRODUCT_PROPERTY_OVERRIDES += \
     sys.use_fifo_ui=1
